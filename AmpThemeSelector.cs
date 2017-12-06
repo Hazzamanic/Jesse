4072861a6af970640b9d3aa12aac657d2dff7ffb
Harry Westbrook <breakitup@hotmail.co.uk>
2017-12-06 18:14:25 +0000

    updates

---
+++ AmpThemeSelector.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Routing;
+using Hazza.Amp.Services;
+using Orchard.Themes;
+
+namespace AmpBase {
+    public class AmpThemeSelector : IThemeSelector {
+        private readonly IAmpAccessor _ampAccessor;
+
+        public AmpThemeSelector(IAmpAccessor ampAccessor) {
+            _ampAccessor = ampAccessor;
+        }
+
+        public ThemeSelectorResult GetTheme(RequestContext context) {
+            if (_ampAccessor.IsAmpRequest)
+                return new ThemeSelectorResult() {
+                    Priority = 1,
+                    ThemeName = "Jesse"
+                };
+
+            return null;
+        }
+    }
+}
